7834b442f06579e6335d9c017e806efed9e0b759
NoobGrinder420 <d1h2a3n4v5i6n@gmail.com>
2025-02-01 23:27:05 +0800

    added greeting

---
+++ greeting/greeting.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	#define int long long
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	
+	string S;
+	getline(cin, S);
+	
+	cout << S << " ni hao, xin nian kuai le!";
+	
+	
+	return 0;
+}
