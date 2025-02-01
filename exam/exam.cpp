97c5d2b2ed505e3fb79175adf3a1bc0925dac7f2
NoobGrinder420 <d1h2a3n4v5i6n@gmail.com>
2025-02-01 23:13:25 +0800

    added exam

---
+++ exam/exam.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x;
+  cin >> x;
+
+  if(x == 1) { 
+    cout << "Hello World";
+  
+  } else if(x == 2) {
+    long long a, b;
+    cin >> a >> b;
+    cout << a*b;
+    
+  }
+
+  return 0;
+
+}
