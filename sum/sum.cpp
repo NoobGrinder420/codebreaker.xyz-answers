b87264413fcf4e4e5fef817cfecd935dc5e2c137
NoobGrinder420 <d1h2a3n4v5i6n@gmail.com>
2025-02-02 21:13:08 +0800

    update

--- sum/sum.cpp
+++ sum/sum.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-//idk how this works either copied from elsewhere
+//idk how this works either copied from man-made intelligence
 
 inline int readInt() {
     int x = 0;
