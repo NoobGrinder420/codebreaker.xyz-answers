861afc9347ef2fa4c7d9dbe5240b2a9ed4aee6f7
NoobGrinder420 <d1h2a3n4v5i6n@gmail.com>
2025-02-02 19:36:38 +0800

    added shamelessad

---
+++ shamelessad/shamelessad.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "Join Shor the Duck’s server, Shor’s Nest!\nAlso check out the collection!";
+	
+	return 0;
+}
+
